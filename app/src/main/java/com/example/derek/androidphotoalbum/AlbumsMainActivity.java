5ed6e75d7c8c6104f8bde09760124c6a71c1a76b
Dali He <dali2bno1@gmail.com>
2016-04-27 12:02:45 -0400

    add few things forget to add before

--- app/src/main/java/com/example/derek/androidphotoalbum/AlbumsMainActivity.java
+++ app/src/main/java/com/example/derek/androidphotoalbum/AlbumsMainActivity.java
@@ -3,6 +3,7 @@ package com.example.derek.androidphotoalbum;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class AlbumsMainActivity extends AppCompatActivity {
 
@@ -15,6 +16,14 @@ public class AlbumsMainActivity extends AppCompatActivity {
     public boolean onCreateOptionMenu(Menu menu){
         getMenuInflater().inflate(R.menu.add_delete_menu,menu);
         return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
